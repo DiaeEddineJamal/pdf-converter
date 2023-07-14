4ce97558faaa226e936797e22fc702256b554439
Luziv the stoic <86086748+DiaeEddineJamal@users.noreply.github.com>
2023-07-14 09:26:26 +0100

    Update doc2pdf.py

--- doc2pdf.py
+++ doc2pdf.py
@@ -3,17 +3,18 @@ from docx import Document
 from docx2pdf import convert
 
 def convert_to_pdf(file):
-    # Convert the document to a DOCX file
-    docx_file = f"{file.name}.docx"
+    # Convert the document to a DOCX file in memory
+    docx_file = file.name + ".docx"
     document = Document(file)
-    document.save(docx_file)
+    docx_data = io.BytesIO()
+    document.save(docx_data)
+    docx_data.seek(0)
 
-    # Convert the DOCX file to PDF
-    pdf_file = f"{file.name}.pdf"
-    convert(docx_file, pdf_file)
+    # Convert the DOCX file to PDF in memory
+    pdf_data = convert(docx_data)
 
-    # Return the PDF file path
-    return pdf_file
+    # Return the PDF file data
+    return pdf_data
 
 # Streamlit app
 def main():
@@ -24,10 +25,10 @@ def main():
 
     if file is not None:
         if st.button("Convert"):
-            pdf_file = convert_to_pdf(file)
+            pdf_data = convert_to_pdf(file)
 
             # Download link for the converted PDF file
-            st.download_button("Download PDF", pdf_file)
+            st.download_button("Download PDF", pdf_data, file.name + ".pdf")
 
 if __name__ == "__main__":
     main()
